f4dab2b28c2d666861ff683bd74a320a3455f17a
FQUIHI Faiçal <134364426+FaicFqui@users.noreply.github.com>
2025-01-19 19:10:48 +0100

    Mise jour code

--- src/Earth.java
+++ src/Earth.java
@@ -5,6 +5,7 @@ import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.PhongMaterial;
 import javafx.scene.shape.Sphere;
+import javafx.scene.transform.Affine;
 import javafx.scene.transform.Rotate;
 
 import java.util.ArrayList;
@@ -13,14 +14,13 @@ import java.util.Objects;
 public class Earth extends Group{
 
     public Sphere sphere;
-    public Rotate ry;
-    private ArrayList<Sphere> yellowSphere;
+    public  Rotate ry;
+    public ArrayList<Sphere> yellowSphere;
 
     public Group root;
 
     public Earth(){
         this.sphere = new Sphere(300);
-
 
         this.sphere.setTranslateX(0);
         this.sphere.setTranslateY(0);
@@ -39,15 +39,12 @@ public class Earth extends Group{
             System.out.println("L'image n'a pas pu être chargée. Chemin incorrect ou image manquante.");
         }
 
-        Rotate rotateY = new Rotate(45, Rotate.Y_AXIS); // Pour centrer la France
-        sphere.getTransforms().addAll(rotateY);
+        this.ry = new Rotate(0, Rotate.Y_AXIS);
+        sphere.getTransforms().addAll(ry);
 
+        
 
-        //rotation de la terre autour de l'axe Y
-        /*ry = new Rotate(0, Rotate.Y_AXIS);
-        sphere.getTransforms().add(ry);
-
-        AnimationTimer timer = new AnimationTimer() {
+       /*AnimationTimer timer = new AnimationTimer() {
             private long startTime = -1;
             private static final double FULL_ROTATION = 360.0; // 360 degrés pour un tour complet
             private static final double DURATION = 15_000_000_000L; // 15 secondes en nanosecondes (15 * 10^9)
@@ -68,36 +65,76 @@ public class Earth extends Group{
                 if (elapsedNanos >= DURATION) {
                     startTime = now;
                 }
+
             }
         };
-
         timer.start();*/
+
+        //ry = new Rotate(45, Rotate.Y_AXIS);
+        //sphere.getTransforms().add(ry);
 
         this.getChildren().add(sphere);
 
     }
+
+
 
     public Sphere creatSphere(double rayon){
         this.sphere = new Sphere(rayon);
         return sphere;
     }
 
-    public void displayRedSphere(double x, double y, double z){
+    public void displayRedSphere(double x, double y) {
+        // Calcul initial des coordonnées XYZ sans rotation
+        /*double[] initialCoords = convertLatLongToLocalXYZ(latitude, longitude, 300);
+        Point3D IP = new Point3D(initialCoords[0], initialCoords[1], initialCoords[2]);
 
+        Point3D RP = rotatePointY(IP,45);*/
+
+        // Créer la sphère rouge et positionner aux coordonnées corrigées
         Sphere sphere_rouge = new Sphere(2);
         sphere_rouge.setMaterial(new PhongMaterial(Color.RED));
-
-        //Point3D newLocalCoordinates = terre.getEarth().sceneToLocal(point3D);
-
-        // Appliquer la position calculée à la sphère rouge
         sphere_rouge.setTranslateX(x);
         sphere_rouge.setTranslateY(y);
-        sphere_rouge.setTranslateZ(z);
+        sphere_rouge.setTranslateZ(-300);
 
+        //System.out.println("x = "+ IP.getX()+" x' = "+ RP.getX());
+        //System.out.println("z = "+ IP.getZ()+" z' = "+ RP.getZ());
+        //System.out.println("x_fonc = "+ initialCoords[0]+" y' = "+ initialCoords[1]+ " z= "+initialCoords[2]);
+        //System.out.println("x_3D = "+ IP.getX()+" y' = "+ IP.getY()+ " z= "+IP.getZ());
 
-
+        // Ajouter la sphère rouge à la grande sphère
         this.getChildren().add(sphere_rouge);
+    }
 
+    public Point3D rotatePointY(Point3D point, double angleInDegrees) {
+        double angleInRadians = Math.toRadians(angleInDegrees);
+
+        double x = point.getX();
+        double y = point.getY();
+        double z = point.getZ();
+
+        // Calcul du nouveau x et z après rotation
+        double newX = Math.cos(angleInRadians) * x + Math.sin(angleInRadians) * (-300);
+        double newY = y;
+        double newZ = -Math.sin(angleInRadians) * x + Math.cos(angleInRadians) * (-300);
+
+        return new Point3D(newX, newY, newZ);
+    }
+
+
+
+    public static double[] convertLatLongToLocalXYZ(double latitude, double longitude, double radius) {
+
+
+        double long_rad = Math.toRadians(longitude);
+        double lat_rad = Math.toRadians(latitude);
+
+        double x = radius * Math.cos(lat_rad) * Math.sin(long_rad);
+        double y = -(radius * Math.sin(lat_rad))/1.5;
+        double z = -radius * Math.cos(lat_rad)*Math.sin(long_rad);
+
+        return new double[]{x, y, z};
     }
 
 
--- src/Interf.java
+++ src/Interf.java
@@ -6,16 +6,14 @@ import javafx.geometry.Point2D;
 import javafx.geometry.Point3D;
 import javafx.scene.*;
 import javafx.scene.image.Image;
+import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.PhongMaterial;
 import javafx.scene.shape.Sphere;
-import javafx.scene.transform.Rotate;
-import javafx.scene.transform.Transform;
-import javafx.scene.transform.Translate;
+import javafx.scene.transform.*;
 import javafx.stage.Stage;
 import javafx.util.Duration;
-import javafx.scene.transform.Scale;
 import javafx.scene.input.PickResult;
 
 
@@ -42,59 +40,52 @@ public class Interf extends Application {
         World w = new World("data/airport-codes_no_comma.csv");
 
 
-        terre.getEarth().setOnMouseClicked(event -> {
-            PickResult pickResult = event.getPickResult();
-            if (pickResult != null && pickResult.getIntersectedNode() == terre.getEarth()) {
-                Point3D localCoordinates = pickResult.getIntersectedPoint(); // Coordonnées locales du point cliqué
-                Point2D point2D = pickResult.getIntersectedTexCoord();
-                double[] geoCoords = getGeographicalCoordinates(point2D);
-                double longitude = geoCoords[1];
-                double latitude = geoCoords[0];
-                if (geoCoords != null) {
-                    System.out.println("picresul " + pickResult);
+        terre.addEventHandler(MouseEvent.ANY, event -> {
+            // Vérifier si c'est un clic droit
+            if (event.getButton() == MouseButton.SECONDARY && event.getEventType() == MouseEvent.MOUSE_CLICKED) {
 
-                    System.out.println("Latitude_cliquée: " + geoCoords[0] + ", Longitude_cliquée: " + geoCoords[1]);
-                    //System.out.println("X_local: " + localCoordinates.getX() + ", Y_local: " + localCoordinates.getY() + " Z_local: " + localCoordinates.getZ());
+                // Obtenir l'élément sur lequel l'événement a été déclenché (le globe)
+                PickResult pickResult = event.getPickResult();
 
-                    Aeroport plus_proche = w.findNearestAirport(longitude, latitude);
-                    System.out.println("L'aeroport le plus proche de l'endroit cilqué : " + plus_proche);
-                    System.out.println("aeroport lat = "+ plus_proche.getLatitude() + " long = "+plus_proche.getLongitude());
+                // Vérifier que le pickResult n'est pas null et que le nœud intersecté est la Terre
+                if (pickResult.getIntersectedNode() != null && pickResult.getIntersectedNode() == terre.getEarth()) {
+                    // Récupérer les coordonnées locales du point cliqué
+                    Point3D localCoordinates = pickResult.getIntersectedPoint();  // Coordonnées locales du point cliqué
+                    Point2D point2D = pickResult.getIntersectedTexCoord(); // Coordonnées 2D de la texture
 
+                    // Conversion des coordonnées de texture en latitude et longitude
+                    double[] geoCoords = getGeographicalCoordinates(point2D);
+                    double longitude = geoCoords[1];
+                    double latitude = geoCoords[0];
 
-                    double[] vecteur = convertLatLongToLocalXYZ(plus_proche.getLatitude(), plus_proche.getLongitude(), 300);
-                    double x = vecteur[0];
-                    double y = vecteur[1]/1.5;
-                    double z = -300;
+                    if (geoCoords != null) {
+                        System.out.println("PickResult: " + pickResult);
 
-                   // Appliquer la transformation de rotation inverse pour corriger la position de la petite sphère
-                    Point3D point3D = new Point3D(x, y, z);
+                        System.out.println("Latitude cliquée: " + latitude + ", Longitude cliquée: " + longitude);
 
-                    /*Point3D transformedPoint = terre.ry.transform(point3D); // Applique la transformation
-                    double newX = transformedPoint.getX();
-                    double newY = transformedPoint.getY();
-                    double newZ = transformedPoint.getZ();*/
-                    // Transformer les coordonnées locales en coordonnées globales (après rotation)
-                    //Point3D newLocalCoordinates = terre.getEarth().sceneToLocal(point3D);
-                    //System.out.println("x_local_apres : "+ point3D.getX() +" y_local_apres: "+point3D.getY()+ " z_local_apres : " + point3D.getZ());
+                        // Recherche de l'aéroport le plus proche basé sur la latitude et longitude
+                        Aeroport plusProche = w.findNearestAirport(longitude, latitude);
+                        System.out.println("L'aéroport le plus proche de l'endroit cliqué : " + plusProche);
+                        System.out.println("Aéroport lat = " + plusProche.getLatitude() + " long = " + plusProche.getLongitude());
 
-                    // Maintenant, transformer les coordonnées de la scène en coordonnées locales pour placer la petite sphère
-                    //Point3D newLocalCoordinates = terre.getEarth().sceneToLocal(sceneCoordinates);
-                    System.out.println("x_aero_cal : "+ x +" y_aero_cal: "+y+ " z_aero_cal : " + z);
+                        // Conversion des coordonnées de l'aéroport en coordonnées locales (XYZ)
+                        double[] vecteur = convertLatLongToLocalXYZ(plusProche.getLatitude(), plusProche.getLongitude(), 300);
+                        double x = vecteur[0];
+                        double y = vecteur[1];
+                        double z = vecteur[2];
 
-                    Point3D p = new Point3D(-34.94683061221838, -113.3332354661558, -275.56178394133974);
-                    // Convertir les coordonnées locales en coordonnées de scène
-                    Point3D globalCoordinates = terre.getEarth().sceneToLocal(point3D);
-                    //System.out.println("x_local_cal : "+ point3D.getX() +" y_local_cal: "+point3D.getY()+ " z_local_cal : " + point3D.getZ());
-                    System.out.println("x_global : "+ globalCoordinates.getX() +" y_global: "+globalCoordinates.getY()+ " z_global : "+ globalCoordinates.getZ());
+                        // Appliquer la transformation de rotation inverse pour corriger la position de la petite sphère
+                        Point3D point3D = new Point3D(x, y, z);
 
-                    // Appliquer la transformation inverse pour repasser dans le repère de la Terre
-                    Point3D localAfterRotation = terre.getEarth().localToScene(globalCoordinates);
-                    // Afficher la petite sphère rouge aux nouvelles coordonnées locales
-                    terre.displayRedSphere(localAfterRotation.getX(),y, -300);
+                        System.out.println("x_aero_cal : " + x + " y_aero_cal: " + y + " z_aero_cal : " + z);
 
+                        // Afficher la petite sphère rouge aux nouvelles coordonnées locales sur le globe
+                        terre.displayRedSphere(x, y);
+                    }
                 }
             }
         });
+
 
         Scene scene = new Scene(terre, 700, 700, true);
         scene.setCamera(camera);
@@ -112,34 +103,15 @@ public class Interf extends Application {
         double lat_rad = Math.toRadians(latitude);
 
         double x = radius * Math.cos(lat_rad) * Math.sin(long_rad);
-        double y = -(radius * Math.sin(lat_rad));
+        double y = -(radius * Math.sin(lat_rad))/1.5;
         double z = -radius * Math.cos(lat_rad)*Math.sin(long_rad);
 
         return new double[]{x, y, z};
     }
 
     private double[] getGeographicalCoordinates(Point2D localCoordinates) {
-        // Normaliser les coordonnées locales pour obtenir un vecteur unitaire (rayon de la Terre)
-        /*double normalizedX = localCoordinates.getX() / 300;
-        double normalizedY = localCoordinates.getY() / 300;
-        double normalizedZ = localCoordinates.getZ() / 300;
-
-
-        // Convertir les coordonnées normalisées en un intervalle de 0 à 1
-        double xNormalized = (normalizedX + 1) / 2;
-        double yNormalized = (1 - normalizedY) / 2;
-
-        // Calculer la longitude (-180° à +180°)
-        double longitude = (xNormalized * 360 - 180); //   div par 3.2 : coef de corection empirique
-
-        // Calculer la latitude (+90° à -90°)
-        double latitude = yNormalized * 180 - 90;*/
-
         double latitude = 180 * (0.5 - localCoordinates.getY());
         double longitude = 360 * (localCoordinates.getX()-0.5);
-
-
-
         return new double[]{latitude, longitude};
     }
 
